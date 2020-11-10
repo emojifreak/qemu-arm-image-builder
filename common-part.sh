26e024019439868187e996fa7a20b7e0a85a501c
emojifreak <53028623+emojifreak@users.noreply.github.com>
2020-11-11 08:49:12 +0900

    Assume Fedora's OVMF

--- common-part.sh
+++ common-part.sh
@@ -179,18 +179,21 @@ elif [ $ARCH = armhf -o $ARCH = armel ]; then
   OVMFCODE=/usr/share/AAVMF/AAVMF32_CODE.fd
   OVMFDATA=/usr/share/AAVMF/AAVMF32_VARS.fd
 elif [ $ARCH = amd64 ]; then
-  # For UEFI secure boot, use OVMF_CODE_4M.ms.fd and OVMF_VARS_4M.ms.fd
-  if [ -r /usr/share/OVMF/OVMF_VARS_4M.ms.fd ]; then
+  # Debian ovmf packages are broken
+  if [ -r /usr/local/share/OVMF-Fedora/OVMF_CODE.secboot.fd ]; then
+    OVMFCODE=/usr/local/share/OVMF-Fedora/OVMF_CODE.secboot.fd
+    OVMFDATA=/usr/local/share/OVMF-Fedora/OVMF_VARS.secboot.fd
+  elif [ -r /usr/share/OVMF/OVMF_VARS_4M.ms.fd ]; then
     OVMFCODE=/usr/share/OVMF/OVMF_CODE_4M.fd
-    OVMFDATA=/usr/share/OVMF/OVMF_VARS_4M.ms.fd
+    OVMFDATA=/usr/share/OVMF/OVMF_VARS_4M.fd
   else
     OVMFCODE=/usr/share/OVMF/OVMF_CODE.fd
-    OVMFDATA=/usr/share/OVMF/OVMF_VARS.ms.fd
+    OVMFDATA=/usr/share/OVMF/OVMF_VARS.fd
   fi    
 elif [ $ARCH = i386 ]; then 
   echo "Warning: UEFI roms for i386 is not yet available in Debian."
-  OVMFCODE=/usr/share/OVMF/OVMF32_CODE_4M.ms.fd
-  OVMFDATA=/usr/share/OVMF/OVMF32_VARS_4M.ms.fd
+  OVMFCODE=/usr/local/share/OVMF-Fedora/OVMF32_CODE.secboot.fd
+  OVMFDATA=/usr/local/share/OVMF-Fedora/OVMF32_VARS.secboot.fd
 else
   echo "Unknown architecture and I don't know a suitable UEFI rom..."
 fi
@@ -281,7 +284,7 @@ cat <<EOF
 
 To start the guest run the following commands:
 cp $OVMFDATA $COPY_EFIVARS
-$QEMU $KVM $GRAPHICS -net nic,model=virtio -net user -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0,id=rng-device0 -drive if=virtio,file=${IMGFILE},index=0,format=raw -drive if=pflash,format=raw,unit=0,file=${OVMFCODE},readonly=on  -drive if=pflash,format=raw,unit=1,file=$COPY_EFIVARS -m 1024 -cpu $CPU -machine $MACHINE
+$QEMU $KVM $GRAPHICS -net nic,model=virtio -net user -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0,id=rng-device0 -drive if=virtio,file=${IMGFILE},index=0,format=raw,discard=unmap,detect-zeroes=unmap -drive if=pflash,format=raw,unit=0,file=${OVMFCODE},readonly=on  -drive if=pflash,format=raw,unit=1,file=$COPY_EFIVARS -m 1024 -cpu $CPU -machine $MACHINE
 EOF
 
 exit 0
